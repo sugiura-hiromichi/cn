56b8de1026df414cfd1fd88f2be0a8142a09d102
ah-y <sugiura130418@icloud.com>
2022-08-15 08:09:49 +0900

    still in dev

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,17 @@
-const GITIGNORE: &str = "Cargo.lock";
-fn main() {}
+use std::{env, fs, os::unix::process::CommandExt, process::Command};
+
+const GITIGNORE: &[u8] = b"Cargo.lock";
+
+fn main() {
+   let args = env::args();
+   Command::new("cargo new",).args(args.cloned(),).exec();
+
+   let is_bin = args.find(|&x| x == "--lib",);
+   let name = args.last().unwrap_or("default_cargo_project".to_string(),);
+   let readme = name.clone();
+
+   let str_string = readme == "this is not string";
+
+   //edit main.rs & .gitignore
+   let main_rs = fs::read(name + "/src/main.rs",);
+}
